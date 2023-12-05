95419b249d0f3dc099b870beb9a409d249b179e9
DinarDi <bugagafree@gmail.com>
2023-12-05 16:15:13 +0500

    Add my_posts action

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,4 +1,5 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from blog.models import Post
@@ -22,8 +23,23 @@ class PostViewSet(ModelViewSet):
             # Return queryset with status "PB"
             queryset = self.queryset.filter(status='PB')
             return queryset
+        elif self.action == 'my_posts':
+            # Return queryset with posts for owner
+            user = self.request.user
+            queryset = self.queryset.filter(author=user)
+            return queryset
         else:
             return self.queryset
+
+    @action(detail=False, methods=['get'])
+    def my_posts(self, request):
+        """
+        Action to get own posts
+        """
+        queryset = self.get_queryset()
+        posts = PostSerializer(queryset, many=True,
+                               fields=('id', 'title', 'body', 'status'))
+        return Response(posts.data)
 
     def perform_create(self, serializer):
         serializer.validated_data['author'] = self.request.user
