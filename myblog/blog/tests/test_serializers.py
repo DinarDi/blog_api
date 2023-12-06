525f1b403a30fdf0806902a91dffa6ff1ad7f666
DinarDi <bugagafree@gmail.com>
2023-12-06 19:59:28 +0500

    Add test for PostDetailSerializer

--- myblog/blog/tests/test_serializers.py
+++ myblog/blog/tests/test_serializers.py
@@ -2,8 +2,8 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from django.utils import dateparse
 
-from blog.models import Post
-from blog.serializers import AuthorInfoSerializer, PostSerializer
+from blog.models import Post, Comment
+from blog.serializers import AuthorInfoSerializer, PostSerializer, PostDetailSerializer
 
 
 class AuthorInfoSerializerTestCase(TestCase):
@@ -105,4 +105,106 @@ class PostSerializerTestCase(TestCase):
         for data in serialized_data:
             data['created'] = dateparse.parse_datetime(data['created'])
             data['updated'] = dateparse.parse_datetime(data['updated'])
+        self.assertEqual(expected_data, serialized_data)
+
+
+class PostDetailSerializerTestCase(TestCase):
+    def setUp(self):
+        self.test_user_1 = User.objects.create(username='user_1')
+        self.test_user_2 = User.objects.create(username='user_2')
+
+        self.post_1 = Post.objects.create(title='Some post', body='Some body',
+                                          author=self.test_user_1, status='PB')
+
+        self.comment_1 = Comment.objects.create(author=self.test_user_2, post=self.post_1,
+                               body='Comment 1')
+        self.comment_2 = Comment.objects.create(author=self.test_user_2, post=self.post_1,
+                               body='Comment 2')
+
+    def test_post_detail_serializer_with_fields(self):
+        post = Post.objects.get(id=self.post_1.id)
+        serialized_data = PostDetailSerializer(post, fields=(
+            'id', 'title',
+            'body', 'comments',
+        )).data
+        expected_data = {
+            'id': self.post_1.id,
+            'title': 'Some post',
+            'body': 'Some body',
+            'comments': [
+                {
+                    'id': self.comment_1.id,
+                    'author': {
+                        'id': self.test_user_2.id,
+                        'first_name': self.test_user_2.first_name,
+                        'last_name': self.test_user_2.last_name
+                    },
+                    'body': self.comment_1.body,
+                    'created': self.comment_1.created,
+                    'updated': self.comment_1.updated
+                },
+                {
+                    'id': self.comment_2.id,
+                    'author': {
+                        'id': self.test_user_2.id,
+                        'first_name': self.test_user_2.first_name,
+                        'last_name': self.test_user_2.last_name
+                    },
+                    'body': self.comment_2.body,
+                    'created': self.comment_2.created,
+                    'updated': self.comment_2.updated
+                },
+            ]
+        }
+        for comment in serialized_data['comments']:
+            comment['created'] = dateparse.parse_datetime(comment['created'])
+            comment['updated'] = dateparse.parse_datetime(comment['updated'])
+        self.assertEqual(expected_data, serialized_data)
+
+    def test_post_detail_serializer_without_fields(self):
+        post = Post.objects.get(id=self.post_1.id)
+        serialized_data = PostDetailSerializer(post).data
+        expected_data = {
+            'id': self.post_1.id,
+            'author': {
+                'id': self.test_user_1.id,
+                'first_name': self.test_user_1.first_name,
+                'last_name': self.test_user_1.last_name
+            },
+            'title': 'Some post',
+            'body': 'Some body',
+            'status': 'PB',
+            'created': self.post_1.created,
+            'updated': self.post_1.updated,
+            'comments': [
+                {
+                    'id': self.comment_1.id,
+                    'author': {
+                        'id': self.test_user_2.id,
+                        'first_name': self.test_user_2.first_name,
+                        'last_name': self.test_user_2.last_name
+                    },
+                    'body': self.comment_1.body,
+                    'created': self.comment_1.created,
+                    'updated': self.comment_1.updated
+                },
+                {
+                    'id': self.comment_2.id,
+                    'author': {
+                        'id': self.test_user_2.id,
+                        'first_name': self.test_user_2.first_name,
+                        'last_name': self.test_user_2.last_name
+                    },
+                    'body': self.comment_2.body,
+                    'created': self.comment_2.created,
+                    'updated': self.comment_2.updated
+                },
+            ]
+        }
+
+        serialized_data['created'] = dateparse.parse_datetime(serialized_data['created'])
+        serialized_data['updated'] = dateparse.parse_datetime(serialized_data['updated'])
+        for comment in serialized_data['comments']:
+            comment['created'] = dateparse.parse_datetime(comment['created'])
+            comment['updated'] = dateparse.parse_datetime(comment['updated'])
         self.assertEqual(expected_data, serialized_data)
